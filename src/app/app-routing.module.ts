a839c0beae3986f9e9e03987113c704287b389f3
santosh kolekar <santoshkolekar165@gmail.com>
2021-03-21 12:41:52 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ConnectComponent } from './connect/connect.component';
+import { UserComponent } from './user/user.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: 'user',
+    component: UserComponent
+    },
+    {
+      path: 'userdetails',
+      component: UserdetailsComponent
+      },
+      {
+        path: 'connect',
+        component: ConnectComponent
+      }
+   
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
+import { UserdetailsComponent } from './userdetails/userdetails.component';
+import { ContactComponent } from './contact/contact.component';
+import { ConnectComponent } from './connect/connect.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    UserDetailComponent,
+    UserdetailsComponent,
+    ContactComponent,
+    ConnectComponent
   ],
   imports: [
     BrowserModule,
